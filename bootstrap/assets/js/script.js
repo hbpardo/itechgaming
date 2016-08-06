8fa7b2e2bad873d674af8745f027c8b72a58cf7c
hbpardo <henster.pardo@itechgaming.com>
2016-08-06 16:46:30 +0800

    Updated scripts for toggle navbar and others

--- bootstrap/assets/js/script.js
+++ bootstrap/assets/js/script.js
@@ -28,9 +28,64 @@ $(document).ready(function(){
 			} else {
 				$(this).html(cont);
 			}
-
-			console.log(txt + ' ' +cont);
 		});
 	});
 
+	//show hide recomendations
+	var showbtn = $('#show-btn');
+	var hidebtn = $('#close-btn');
+	var banner = $('#rec-banner');
+
+	hidebtn.click(function(){
+		showbtn.show();
+		banner.fadeOut('fast');
+		$(this).hide();
+	});
+
+	showbtn.click(function(){
+		hidebtn.show();
+		banner.fadeIn('fast');
+		$(this).hide();
+	});
+
+	$('#btn-menu').click(function(){
+		$('#menu').slideToggle();
+		$('i',this).toggleClass("glyphicon-menu-hamburger glyphicon-chevron-up");
+	});
+	
+
 });
+
+
+var id;
+$(window).on('resize', (function() {
+    clearTimeout(id);
+    id = setTimeout(doneResizing, 500);
+    
+}));
+
+function doneResizing(){
+	var win = $(this); //this = window
+  if (win.width() >= 985) {
+  	$('#menu').show();
+
+  	if ($('#btn-menu i').attr('class') === 'glyphicon glyphicon-chevron-up') {
+			$('#btn-menu i').removeClass("glyphicon-chevron-up");
+			$('#btn-menu i').addClass("glyphicon-menu-hamburger");
+		}
+  	
+   }
+  	
+
+  if (win.width() <= 984) {
+  	$('#menu').hide();
+
+  	if ($('#btn-menu i').attr('class') === 'glyphicon glyphicon-chevron-up') {
+			$('#btn-menu i').removeClass("glyphicon-chevron-up");
+			$('#btn-menu i').addClass("glyphicon-menu-hamburger");
+		}
+		
+
+  }
+}
+
